317eac1b35c550d7c1b08c700e4c76ddeff84c10
Esteban L. Gómez <esteban.elig@gmail.com>
2023-01-31 13:04:12 +0100

    Toolbar

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,13 @@ extern crate gio;
 extern crate gtk;
 
 use gio::{ApplicationExt, ApplicationExtManual, ApplicationFlags};
-use gtk::{Application, ApplicationWindow, GtkWindowExt, WidgetExt};
+use gtk::{
+    Application, ApplicationWindow, ContainerExt, GtkWindowExt, SeparatorToolItem, ToolButton,
+    Toolbar, WidgetExt,
+};
 use std::env;
+
+const PLAY_STOCK: &str = "gtk-media-play";
 
 fn main() {
     // gio application
@@ -14,7 +19,28 @@ fn main() {
     application.connect_startup(|application| {
         let window = ApplicationWindow::new(&application);
         window.set_title("Rusic");
-        window.show();
+        let toolbar = Toolbar::new();
+        let open_button = ToolButton::new_from_stock("gtk-open");
+        let previous_button = ToolButton::new_from_stock("gtk-media-previous");
+        let play_button = ToolButton::new_from_stock(PLAY_STOCK);
+        let stop_button = ToolButton::new_from_stock("gtk-media-stop");
+        let next_button = ToolButton::new_from_stock("gtk-media-next");
+        let remove_button = ToolButton::new_from_stock("gtk-remove");
+        let quit_button = ToolButton::new_from_stock("gtk-quit");
+
+        window.add(&toolbar);
+        toolbar.add(&open_button);
+        toolbar.add(&SeparatorToolItem::new());
+        toolbar.add(&previous_button);
+        toolbar.add(&play_button);
+        toolbar.add(&stop_button);
+        toolbar.add(&next_button);
+        toolbar.add(&SeparatorToolItem::new());
+        toolbar.add(&remove_button);
+        toolbar.add(&SeparatorToolItem::new());
+        toolbar.add(&quit_button);
+
+        window.show_all();
     });
     application.connect_activate(|_| {});
     application.run(&env::args().collect::<Vec<_>>());
