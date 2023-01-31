90f9248f164a72da5ad7980b3587ce8e8d9d6746
Esteban L. Gómez <esteban.elig@gmail.com>
2023-01-31 10:26:44 +0100

    First window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate gio;
+extern crate gtk;
+
+use gio::{ApplicationExt, ApplicationExtManual, ApplicationFlags};
+use gtk::{Application, ApplicationWindow, GtkWindowExt, WidgetExt};
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // gio application
+    let application = Application::new("com.github.eligero-rusic", ApplicationFlags::empty())
+        .expect("Application initialization failed");
+
+    // create the window
+    application.connect_startup(|application| {
+        let window = ApplicationWindow::new(&application);
+        window.set_title("Rusic");
+        window.show();
+    });
+    application.connect_activate(|_| {});
+    application.run(&env::args().collect::<Vec<_>>());
 }
