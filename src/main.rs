7913b615af7b570a79b912264bd9bd8e4a701885
Esteban L. Gómez <esteban.elig@gmail.com>
2023-01-31 18:11:28 +0100

    Cover image and cursor

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,12 @@ extern crate gtk;
 
 mod toolbar;
 
-use gtk::{Application, ApplicationWindow, ContainerExt, GtkWindowExt, WidgetExt};
+use gtk::{
+    Adjustment, Application, ApplicationWindow, ContainerExt, GtkWindowExt, Image, ImageExt, Scale,
+    ScaleExt, WidgetExt,
+};
+
+use gtk::Orientation::{Horizontal, Vertical};
 
 use gio::{ApplicationExt, ApplicationExtManual, ApplicationFlags};
 
@@ -13,6 +18,8 @@ use toolbar::MusicToolbar;
 struct App {
     toolbar: MusicToolbar,
     window: ApplicationWindow,
+    cover: Image,
+    adjustment: Adjustment,
 }
 
 impl App {
@@ -20,11 +27,31 @@ impl App {
         let window = ApplicationWindow::new(&application);
         window.set_title("Rusic");
 
+        // Box container, disposes widgets either horizontally or vertically
+        let vbox = gtk::Box::new(Vertical, 0); // 0 separation between the children widgets of the Box container
+        window.add(&vbox);
+
         let toolbar = MusicToolbar::new();
-        window.add(toolbar.toolbar());
+        vbox.add(toolbar.toolbar());
+
+        let cover = Image::new();
+        cover.set_from_file("cover.jpg"); // file in project directory
+        vbox.add(&cover);
+
+        // cursor widget. For now, hardcoded values for adjustment
+        let adjustment = Adjustment::new(0.0, 0.0, 10.0, 0.0, 0.0, 0.0);
+        let scale = Scale::new(Horizontal, &adjustment);
+        scale.set_draw_value(false); // disable show actual value
+        vbox.add(&scale);
+
         window.show_all();
 
-        let app = App { toolbar, window };
+        let app = App {
+            toolbar,
+            window,
+            cover,
+            adjustment,
+        };
 
         app.connect_events();
         app.connect_toolbar_events();
